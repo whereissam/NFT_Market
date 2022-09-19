75574dbe0715e8063e8c6fb15021a2742ae3e7b7
Sam <sam135642@gmail.com>
2022-09-19 22:52:29 +0800

    Feat : UI CSS + Navbar
    tailwind CSS setup
    navbar add switch light & dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { ThemeProvider } from 'next-themes';
 
-export default MyApp
+import { Navbar, Footer } from '../components';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/02244f3392.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+
+);
+
+export default MyApp;
---
+++ pages/create-nft.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// const CreateNFT = () => (
+//   <div>NFT</div>
+// );
+
+// export default CreateNFT;
